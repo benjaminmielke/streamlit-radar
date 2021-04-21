b537c36db7cd88bb520293e22b8deb79fd69a68d
Benjamin Mielke <mielket.ben@gmail.com>
2021-04-21 00:12:50 -0500

    version 2

--- radar_app.py
+++ radar_app.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import requests
 import bs4 as bs
+import time
+import datetime
 
 st.set_page_config(layout='wide', page_title='Radar Regions', page_icon='https://p1.hiclipart.com/preview/994/283/642/rainmeter-tabbed-dock-grey-and-yellow-lightning-icon-png-clipart.jpg')
 
@@ -21,8 +23,12 @@ def create_dct(lst_icao):
         lat = soup.findAll('abbr', {'class': 'latitude'})[0]['title']
         lon = soup.findAll('abbr', {'class': 'longitude'})[0]['title']
         elev = soup.findAll('td', {'colspan': '2'})[9].text
+        try:
+            tz = soup.findAll('abbr', {'class': 'tz'})[0]['title']
+        except IndexError:
+            pass
 
-        dct[f'{lst_icao[a]}'] = {'City': f'{city}', 'Country': f'{country}', 'Lat': f'{lat}', 'Lon': f'{lon}', 'Elevation': f'{elev}'}
+        dct[f'{lst_icao[a]}'] = {'City': city, 'Country': country, 'Lat': round(float(lat), 2), 'Lon': round(float(lon), 2), 'Elevation': elev, 'Timezone': f'UTC{tz}'}
 
     return dct
 
@@ -32,15 +38,53 @@ def icao_button(icao_key, dct):
     '''
 
     ph_title.title(f'{icao_key} - {(dct.get(icao_key)).get("City")}, {(dct.get(icao_key)).get("Country")}')
-    ph_subtitle.markdown(f'Coordinates: {(dct.get(icao_key)).get("Lat")}, {(dct.get(icao_key)).get("Lon")}')
+    ph_subtitle.markdown(f'''**Coordinates:** {(dct.get(icao_key)).get("Lat")}, {(dct.get(icao_key)).get("Lon")}  **Timezone:** {(dct.get(icao_key)).get("Timezone")}
+    **Local TIme: {clock()}''')
     ph_iframe.markdown(f'<iframe src="https://www.rainviewer.com/map.html?loc={(dct.get(icao_key)).get("Lat")},{(dct.get(icao_key)).get("Lon")},8&oFa=1&oC=1&oU=1&oCS=1&oF=1&oAP=0&rmt=1&c=5&o=49&lm=0&th=0&sm=0&sn=1" width="100%" frameborder="0" style="border:0;height:80vh;" allowfullscreen></iframe>', unsafe_allow_html=True)
-    ph_iframe2.markdown(f'<iframe src="https://map.blitzortung.org/#7/{(dct.get(icao_key)).get("Lat")}/{(dct.get(icao_key)).get("Lon")}" width="100%" frameborder="0" style="border:0;height:80vh;" allowfullscreen></iframe>', unsafe_allow_html=True)
+
+
+def radar_button(icao_key, dct):
+    '''
+    '''
+    ph_title.title(f'{icao_key} - {(dct.get(icao_key)).get("City")}, {(dct.get(icao_key)).get("Country")}')
+    ph_subtitle.markdown(f'''**Coordinates:** {(dct.get(icao_key)).get("Lat")}, {(dct.get(icao_key)).get("Lon")}  **Timezone:** {(dct.get(icao_key)).get("Timezone")}''')
+    ph_iframe.markdown(f'<iframe src="https://www.rainviewer.com/map.html?loc={(dct.get(icao_key)).get("Lat")},{(dct.get(icao_key)).get("Lon")},8&oFa=1&oC=1&oU=1&oCS=1&oF=1&oAP=0&rmt=1&c=5&o=49&lm=0&th=0&sm=0&sn=1" width="100%" frameborder="0" style="border:0;height:80vh;" allowfullscreen></iframe>', unsafe_allow_html=True)
 
 
 def lightning_button(icao_key, dct):
     '''
     '''
-    ph_iframe.markdown(f'<iframe src="https://map.blitzortung.org/#2/16.64/19.07" width="100%" frameborder="0" style="border:0;height:80vh;" allowfullscreen></iframe>', unsafe_allow_html=True)
+    ph_title.title(f'{icao_key} - {(dct.get(icao_key)).get("City")}, {(dct.get(icao_key)).get("Country")}')
+    ph_subtitle.markdown(f'**Coordinates:** {(dct.get(icao_key)).get("Lat")}, {(dct.get(icao_key)).get("Lon")}  **Timezone:** {(dct.get(icao_key)).get("Timezone")}')
+    ph_iframe.markdown(f'<iframe src="https://map.blitzortung.org/index.php?MapStyle=3#7/{(dct.get(icao_key)).get("Lat")}/{(dct.get(icao_key)).get("Lon")}" width="100%" frameborder="0" style="border:0;height:80vh;" allowfullscreen></iframe>', unsafe_allow_html=True)
+
+
+def satellite_button(icao_key, dct):
+    '''
+    '''
+    ph_title.title(f'{icao_key} - {(dct.get(icao_key)).get("City")}, {(dct.get(icao_key)).get("Country")}')
+    ph_subtitle.markdown(f'**Coordinates:** {(dct.get(icao_key)).get("Lat")}, {(dct.get(icao_key)).get("Lon")}  **Timezone:** {(dct.get(icao_key)).get("Timezone")}')
+    ph_iframe.markdown(f'<iframe src="https://www.tsohost.com/assets/uploads/blog/under-construction-pages-1-image-library.jpg" width="100%" frameborder="0" style="border:0;height:80vh;" allowfullscreen></iframe>', unsafe_allow_html=True)
+
+
+def region_lightning(coords, zoom):
+    '''
+    '''
+    ph_iframe2.markdown(f'<iframe src="https://map.blitzortung.org/index.php?MapStyle=3#{zoom}/{coords}" width="100%" frameborder="0" style="border:0;height:80vh;" allowfullscreen></iframe>', unsafe_allow_html=True)
+
+
+def region_radar(coords, zoom):
+    '''
+    '''
+    ph_iframe.markdown(f'<iframe src="https://www.rainviewer.com/map.html?loc={coords},{zoom}&oFa=1&oC=1&oU=1&oCS=1&oF=1&oAP=0&rmt=1&c=5&o=49&lm=0&th=0&sm=0&sn=1" width="100%" frameborder="3" style="border:2;height:80vh;" allowfullscreen></iframe>', unsafe_allow_html=True)
+
+
+def clock(dct, icao_key):
+    '''
+    '''
+    local_adjustment = int((dct.get(icao_key)).get("Timezone")[4:6])
+    return (datetime.datetime.utcnow() + datetime.timedelta(hours=local_adjustment)).strftime("%H:%M:%S")
+
 
 
 
@@ -72,22 +116,9 @@ body {
 
 ph_title = st.empty()
 ph_subtitle = st.empty()
+ph_subtitle2 = st.empty()
 ph_iframe = st.empty()
 ph_iframe2 = st.empty()
-
-
-ph_title.title('World')
-ph_iframe.markdown(f'<iframe src="https://www.rainviewer.com/map.html?loc=16.0458,-338.7305,3&oFa=1&oC=1&oU=1&oCS=1&oF=1&oAP=0&rmt=1&c=5&o=49&lm=0&th=1&sm=0&sn=1" width="100%" frameborder="0" style="border:0;height:80vh;" allowfullscreen></iframe>', unsafe_allow_html=True)
-ph_iframe2.markdown(f'<iframe src="https://map.blitzortung.org/#2/16.64/19.07" width="100%" frameborder="0" style="border:0;height:80vh;" allowfullscreen></iframe>', unsafe_allow_html=True)
-
-# if cols_body[1].button('Radar'):
-#     ph_iframe.markdown(f'<iframe src="https://www.rainviewer.com/map.html?loc=16.0458,-338.7305,3&oFa=1&oC=1&oU=1&oCS=1&oF=1&oAP=0&rmt=1&c=5&o=49&lm=0&th=1&sm=0&sn=1" width="100%" frameborder="0" style="border:0;height:80vh;" allowfullscreen></iframe>', unsafe_allow_html=True)
-
-
-
-# cols_data[0].button('Radar')
-# cols_data[1].button('Lightning')
-# cols_data[2].button('Satellite')
 
 st.sidebar.title('Select Region 地域選択')
 region = st.sidebar.selectbox('',
@@ -108,110 +139,102 @@ color: #000000
 # ----------------------------OK1-----------------------------------------------
 if region == 'OK1':
     cols = st.sidebar.beta_columns(3)
-    cols_data = st.sidebar.beta_columns(3)
     if cols[0].button(f'Europe'):
         ph_title.title(f'Europe')
-        ph_iframe.markdown(f'<iframe src="https://www.rainviewer.com/map.html?loc=49.0523,-338.6865,4&oFa=1&oC=1&oU=1&oCS=1&oF=1&oAP=0&rmt=1&c=5&o=49&lm=0&th=1&sm=0&sn=1" width="100%" frameborder="3" style="border:2;height:80vh;" allowfullscreen></iframe>', unsafe_allow_html=True)
+        region_radar('49.0523,-338.6865', 4)
+        region_lightning('54.1/24.82', 3)
     if cols[1].button(f'CONUS'):
         ph_title.title(f'CONUS')
-        ph_iframe.markdown(f'<iframe src="https://www.rainviewer.com/map.html?loc=39.113,-455.7129,4&oFa=1&oC=1&oU=1&oCS=1&oF=1&oAP=0&rmt=1&c=5&o=49&lm=0&th=1&sm=0&sn=1" width="100%" frameborder="0" style="border:0;height:80vh;" allowfullscreen></iframe>', unsafe_allow_html=True)
+        region_radar('39.113,-455.7129', 4)
+        region_lightning('38.24/-96.91', 4)
     if cols[2].button(f'Oceania'):
         ph_title.title(f'Oceania')
-        ph_iframe.markdown(f'<iframe src="https://www.rainviewer.com/map.html?loc=-28.5942,-575.1563,4&oFa=1&oC=1&oU=1&oCS=1&oF=1&oAP=0&rmt=1&c=5&o=49&lm=0&th=1&sm=0&sn=1" width="100%" frameborder="0" style="border:0;height:80vh;" allowfullscreen></iframe>', unsafe_allow_html=True)
+        region_radar('-28.5942,-575.1563', 4)
+        region_lightning('-30.11/142.73', 3.42)
     i = 0
     while i <= len(dct_ok1)-1:
-        cols = st.sidebar.beta_columns(3)
-        cols_data = st.sidebar.beta_columns(3)
-        try:
-            if cols[0].button(f'{list(dct_ok1.keys())[i]}'):
-                icao_button(lst_ok1[i], dct_ok1)
-            # if cols_data[0].button('Lightning'):
-            #     lightning_button(lst_ok1[i], dct_ok1)
-            if cols[1].button(f'{list(dct_ok1.keys())[i+1]}'):
-                icao_button(lst_ok1[i+1], dct_ok1)
-            if cols[2].button(f'{list(dct_ok1.keys())[i+2]}'):
-                icao_button(lst_ok1[i+2], dct_ok1)
-        except IndexError:
-            pass
-        i = i+3
+        cols = st.sidebar.beta_columns([.5,.75,1,1])
+        cols[0].markdown(f'**{list(dct_ok1.keys())[i]}**')
+        if cols[1].button('Radar', key=i+10000):
+            radar_button(lst_ok1[i], dct_ok1)
+        if cols[2].button(f'Lightning', key=i+10001):
+            lightning_button(lst_ok1[i], dct_ok1)
+        if cols[3].button(f'Satellite', key=i+10002):
+            satellite_button(lst_ok1[i], dct_ok1)
+        i = i+1
 
 # ----------------------------OK2------------------------------------------------
 if region == 'OK2':
-    cols = st.sidebar.beta_columns(3)
+    cols = st.sidebar.beta_columns([.82,1,1])
     if cols[0].button(f'Korea'):
         ph_title.title(f'Korea')
-        ph_iframe.markdown(f'<iframe src="https://www.rainviewer.com/map.html?loc=36.7521,-233.1189,7&oFa=1&oC=1&oU=1&oCS=1&oF=1&oAP=0&rmt=1&c=5&o=49&lm=0&th=1&sm=0&sn=1" width="100%" frameborder="0" style="border:0;height:80vh;" allowfullscreen></iframe>', unsafe_allow_html=True)
+        region_radar('36.7521,-233.1189', 7)
+        region_lightning('37.562/127.398', 5.95)
     if cols[1].button(f'N China'):
         ph_title.title(f'NE China')
-        ph_iframe.markdown(f'<iframe src="https://www.rainviewer.com/map.html?loc=42.7228,-239.4141,6&oFa=1&oC=1&oU=1&oCS=1&oF=1&oAP=0&rmt=1&c=5&o=49&lm=0&th=1&sm=0&sn=1" width="100%" frameborder="0" style="border:0;height:80vh;" allowfullscreen></iframe>', unsafe_allow_html=True)
+        ph_iframe.markdown(f'<iframe src="https://www.rainviewer.com/map.html?loc=42.7228,-239.4141,6&oFa=1&oC=1&oU=1&oCS=1&oF=1&oAP=0&rmt=1&c=5&o=49&lm=0&th=0&sm=0&sn=1" width="100%" frameborder="0" style="border:0;height:80vh;" allowfullscreen></iframe>', unsafe_allow_html=True)
     if cols[2].button(f'E China'):
         ph_title.title(f'E China')
-        ph_iframe.markdown(f'<iframe src="https://www.rainviewer.com/map.html?loc=35.6662,-246.6431,6&oFa=1&oC=1&oU=1&oCS=1&oF=1&oAP=0&rmt=1&c=5&o=49&lm=0&th=1&sm=0&sn=1" width="100%" frameborder="0" style="border:0;height:80vh;" allowfullscreen></iframe>', unsafe_allow_html=True)
+        ph_iframe.markdown(f'<iframe src="https://www.rainviewer.com/map.html?loc=35.6662,-246.6431,6&oFa=1&oC=1&oU=1&oCS=1&oF=1&oAP=0&rmt=1&c=5&o=49&lm=0&th=0&sm=0&sn=1" width="100%" frameborder="0" style="border:0;height:80vh;" allowfullscreen></iframe>', unsafe_allow_html=True)
     i = 0
     while i <= len(dct_ok2)-1:
-        cols = st.sidebar.beta_columns(3)
-        try:
-            if cols[0].button(f'{list(dct_ok2.keys())[i]}'):
-                icao_button(lst_ok2[i], dct_ok2)
-            if cols[1].button(f'{list(dct_ok2.keys())[i+1]}'):
-                icao_button(lst_ok2[i+1], dct_ok2)
-            if cols[2].button(f'{list(dct_ok2.keys())[i+2]}'):
-                icao_button(lst_ok2[i+2], dct_ok2)
-        except IndexError:
-            pass
-        i = i+3
+        cols = st.sidebar.beta_columns([.5,.75,1,1])
+        cols[0].markdown(f'**{list(dct_ok2.keys())[i]}**')
+        if cols[1].button('Radar', key=i+10000):
+            radar_button(lst_ok2[i], dct_ok2)
+        if cols[2].button(f'Lightning', key=i+10001):
+            lightning_button(lst_ok2[i], dct_ok2)
+        if cols[3].button(f'Satellite', key=i+10002):
+            satellite_button(lst_ok2[i], dct_ok2)
+        i = i+1
 
 # ----------------------------OK3------------------------------------------------
 if region == 'OK3':
     cols = st.sidebar.beta_columns(3)
     if cols[0].button(f'Indochina'):
         ph_title.title(f'Indochina')
-        ph_iframe.markdown(f'<iframe src="https://www.rainviewer.com/map.html?loc=13.5018,-255.2563,7&oFa=1&oC=1&oU=1&oCS=1&oF=1&oAP=0&rmt=1&c=5&o=49&lm=0&th=1&sm=0&sn=1" width="100%" frameborder="0" style="border:0;height:80vh;" allowfullscreen></iframe>', unsafe_allow_html=True)
+        ph_iframe.markdown(f'<iframe src="https://www.rainviewer.com/map.html?loc=15.316,-256.4209,6&oFa=1&oC=1&oU=1&oCS=1&oF=1&oAP=1&rmt=1&c=5&o=49&lm=0&th=0&sm=0&sn=1" width="100%" frameborder="0" style="border:0;height:80vh;" allowfullscreen></iframe>', unsafe_allow_html=True)
     if cols[1].button(f'S China'):
         ph_title.title(f'SE China')
-        ph_iframe.markdown(f'<iframe src="https://www.rainviewer.com/map.html?loc=22.761,-250.5212,7&oFa=1&oC=1&oU=1&oCS=1&oF=1&oAP=0&rmt=1&c=5&o=49&lm=0&th=1&sm=0&sn=1" width="100%" frameborder="0" style="border:0;height:80vh;" allowfullscreen></iframe>', unsafe_allow_html=True)
+        ph_iframe.markdown(f'<iframe src="https://www.rainviewer.com/map.html?loc=25.4334,-251.3452,6&oFa=1&oC=1&oU=1&oCS=1&oF=1&oAP=1&rmt=1&c=5&o=49&lm=0&th=0&sm=0&sn=1" width="100%" frameborder="0" style="border:0;height:80vh;" allowfullscreen></iframe>', unsafe_allow_html=True)
     if cols[2].button(f'India'):
         ph_title.title(f'India')
-        ph_iframe.markdown(f'<iframe src="https://www.rainviewer.com/map.html?loc=24.3971,-277.2949,6&oFa=1&oC=1&oU=1&oCS=1&oF=1&oAP=0&rmt=1&c=5&o=49&lm=0&th=1&sm=0&sn=1" width="100%" frameborder="0" style="border:0;height:80vh;" allowfullscreen></iframe>', unsafe_allow_html=True)
+        ph_iframe.markdown(f'<iframe src="https://www.rainviewer.com/map.html?loc=24.3971,-277.2949,6&oFa=1&oC=1&oU=1&oCS=1&oF=1&oAP=0&rmt=1&c=5&o=49&lm=0&th=0&sm=0&sn=1" width="100%" frameborder="0" style="border:0;height:80vh;" allowfullscreen></iframe>', unsafe_allow_html=True)
     i = 0
     while i <= len(dct_ok3)-1:
-        cols = st.sidebar.beta_columns(3)
-        try:
-            if cols[0].button(f'{list(dct_ok3.keys())[i]}'):
-                icao_button(lst_ok3[i], dct_ok3)
-            if cols[1].button(f'{list(dct_ok3.keys())[i+1]}'):
-                icao_button(lst_ok3[i+1], dct_ok3)
-            if cols[2].button(f'{list(dct_ok3.keys())[i+2]}'):
-                icao_button(lst_ok3[i+2], dct_ok3)
-        except IndexError:
-            pass
-        i = i+3
+        cols = st.sidebar.beta_columns([.5,.75,1,1])
+        cols[0].markdown(f'**{list(dct_ok3.keys())[i]}**')
+        if cols[1].button('Radar', key=i+10000):
+            radar_button(lst_ok3[i], dct_ok3)
+        if cols[2].button(f'Lightning', key=i+10001):
+            lightning_button(lst_ok3[i], dct_ok3)
+        if cols[3].button(f'Satellite', key=i+10002):
+            satellite_button(lst_ok3[i], dct_ok3)
+        i = i+1
 
 # ----------------------------OK4------------------------------------------------
 if region == 'OK4':
-    cols = st.sidebar.beta_columns(3)
-    if cols[0].button(f'Java'):
-        ph_title.title(f'Java')
-        ph_iframe.markdown(f'<iframe src="https://www.rainviewer.com/map.html?loc=-7.6729,-248.2471,7&oFa=1&oC=1&oU=1&oCS=1&oF=1&oAP=0&rmt=1&c=5&o=49&lm=0&th=1&sm=0&sn=1" width="100%" frameborder="0" style="border:0;height:80vh;" allowfullscreen></iframe>', unsafe_allow_html=True)
-    if cols[1].button(f'Malaysia'):
-        ph_title.title(f'Malaysia')
-        ph_iframe.markdown(f'<iframe src="https://www.rainviewer.com/map.html?loc=1.1755,-251.543,6&oFa=1&oC=1&oU=1&oCS=1&oF=1&oAP=0&rmt=1&c=5&o=49&lm=0&th=1&sm=0&sn=1" width="100%" frameborder="0" style="border:0;height:80vh;" allowfullscreen></iframe>', unsafe_allow_html=True)
-    if cols[2].button(f'Russia'):
+    cols = st.sidebar.beta_columns([.95,1.18,1])
+    if cols[0].button(f'Indonesia'):
+        ph_title.title(f'Indonesia')
+        ph_iframe.markdown(f'<iframe src="https://www.rainviewer.com/map.html?loc=-1.6148,-249.8291,6&oFa=1&oC=1&oU=1&oCS=1&oF=1&oAP=1&rmt=1&c=5&o=49&lm=0&th=0&sm=0&sn=1" width="100%" frameborder="0" style="border:0;height:80vh;" allowfullscreen></iframe>', unsafe_allow_html=True)
+    if cols[1].button(f'Philippines'):
+        ph_title.title(f'Philippines')
+        ph_iframe.markdown(f'<iframe src="https://www.rainviewer.com/map.html?loc=11.7275,-236.1841,6&oFa=1&oC=0&oU=1&oCS=1&oF=1&oAP=1&rmt=1&c=5&o=49&lm=0&th=0&sm=0&sn=1" width="100%" frameborder="0" style="border:0;height:80vh;" allowfullscreen></iframe>', unsafe_allow_html=True)
+    if cols[2].button(f'N China'):
         ph_title.title(f'W Russia')
-        ph_iframe.markdown(f'<iframe src="https://www.rainviewer.com/map.html?loc=58.3672,-314.3848,5&oFa=1&oC=1&oU=1&oCS=1&oF=1&oAP=0&rmt=1&c=5&o=49&lm=0&th=1&sm=0&sn=1" width="100%" frameborder="0" style="border:0;height:80vh;" allowfullscreen></iframe>', unsafe_allow_html=True)
+        ph_iframe.markdown(f'<iframe src="https://www.rainviewer.com/map.html?loc=45.4524,-235.459,6&oFa=1&oC=1&oU=1&oCS=1&oF=1&oAP=1&rmt=1&c=5&o=49&lm=0&th=0&sm=0&sn=1" width="100%" frameborder="0" style="border:0;height:80vh;" allowfullscreen></iframe>', unsafe_allow_html=True)
     i = 0
     while i <= len(dct_ok4)-1:
-        cols = st.sidebar.beta_columns(3)
-        try:
-            if cols[0].button(f'{list(dct_ok4.keys())[i]}'):
-                icao_button(lst_ok4[i], dct_ok4)
-            if cols[1].button(f'{list(dct_ok4.keys())[i+1]}'):
-                icao_button(lst_ok4[i+1], dct_ok4)
-            if cols[2].button(f'{list(dct_ok4.keys())[i+2]}'):
-                icao_button(lst_ok4[i+3], dct_ok4)
-        except IndexError:
-            pass
-        i = i+3
+        cols = st.sidebar.beta_columns([.5,.75,1,1])
+        cols[0].markdown(f'**{list(dct_ok4.keys())[i]}**')
+        if cols[1].button('Radar', key=i+10000):
+            radar_button(lst_ok4[i], dct_ok4)
+        if cols[2].button(f'Lightning', key=i+10001):
+            lightning_button(lst_ok4[i], dct_ok4)
+        if cols[3].button(f'Satellite', key=i+10002):
+            satellite_button(lst_ok4[i], dct_ok4)
+        i = i+1
 
 
 # ----------------------------SJN------------------------------------------------
@@ -228,14 +251,12 @@ if region == 'SJN':
         ph_iframe.markdown(f'<iframe src="https://www.rainviewer.com/map.html?loc=41.2984,-217.8699,7&oFa=1&oC=0&oU=1&oCS=1&oF=1&oAP=0&rmt=1&c=5&o=49&lm=0&th=0&sm=0&sn=1" width="100%" frameborder="0" style="border:0;height:80vh;" allowfullscreen></iframe>', unsafe_allow_html=True)
     i = 0
     while i <= len(dct_sjn)-1:
-        cols = st.sidebar.beta_columns(3)
-        try:
-            if cols[0].button(f'{list(dct_sjn.keys())[i]}'):
-                icao_button(lst_sjn[i], dct_sjn)
-            if cols[1].button(f'{list(dct_sjn.keys())[i+1]}'):
-                icao_button(lst_sjn[i+1], dct_sjn)
-            if cols[2].button(f'{list(dct_sjn.keys())[i+2]}'):
-                icao_button(lst_sjn[i+2], dct_sjn)
-        except IndexError:
-            pass
-        i = i+3
+        cols = st.sidebar.beta_columns([.5,.75,1,1])
+        cols[0].markdown(f'**{list(dct_sjn.keys())[i]}**')
+        if cols[1].button('Radar', key=i+10000):
+            radar_button(lst_sjn[i], dct_sjn)
+        if cols[2].button(f'Lightning', key=i+10001):
+            lightning_button(lst_sjn[i], dct_sjn)
+        if cols[3].button(f'Satellite', key=i+10002):
+            satellite_button(lst_sjn[i], dct_sjn)
+        i = i+1
